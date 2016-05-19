1517a2053836f2de3f673b3c3c4e349a4c9621f8
RITESH SAINJU <RITESH SAINJU>
2016-05-19 16:57:03 +0545

    <parser and sorter done>

--- parser/src/main/java/np/com/rts/internship/beginners/Main.java
+++ parser/src/main/java/np/com/rts/internship/beginners/Main.java
@@ -5,16 +5,21 @@ import java.util.*;
 
 public class Main {
 
-  private String parseThis = "20160519150000-STX,6-3.0-CONF4";
+	  private String parseThis = "20160519150000-STX,6-3.0-CONF4";
 
-  // Parsed value must be inserted into this variable
-  double value;
+	  // Parsed value must be inserted into this variable
+	  double value;
 
-  public Main() {
-    // Write your code here
-
-
-  }
+	  public Main() {
+	    // Write your code here
+		  String[] parse=parseThis.split("-");
+		  
+		  System.out.println(parse[2]);
+		  value=Double.parseDouble(parse[2]);
+		  System.out.println(value);
+	  }
+	  
+		  
 
   public static void main(String[] args) {
     new Main();
--- sorter/src/main/java/np/com/rts/internship/beginners/Main.java
+++ sorter/src/main/java/np/com/rts/internship/beginners/Main.java
@@ -18,10 +18,41 @@ public class Main {
   };
 
   public Main() {
-    // Write your code here
+	    // Write your code here
+		  for(int j=0; j<strings.length;j++)
+		  {
+		      for (int i=j+1 ; i<strings.length; i++)
+		      {
+		          if(strings[i].compareTo(strings[j])<0)
+		          {
+		              String temp= strings[j];
+		              strings[j]= strings[i]; 
+		              strings[i]=temp;
 
 
-  }
+		          }
+		      }
+		      //Arrays.sort(strings);
+			   //  for(String a:strings){
+			    //	 System.out.println(strings);
+			     // System.out.println(strings[strings.length-1]);
+			      
+			     }
+		  String last = strings[strings.length-1];
+		  System.out.println(last);
+		  
+		  String max = strings[0];
+		  boolean last1 = true;
+		  for(int i=1;i<strings.length;i++){
+			 if(last.compareTo(strings[i])>0){
+				 last1=false;
+				
+				 }
+				  }
+			
+		 System.out.println(last1);
+			 
+		 }
 
   public static void main(String[] args) {
     new Main();
